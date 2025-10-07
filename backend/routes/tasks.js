4505e233c7ba6c5abd1ff760591aaccca673b2f5
Anbu33 <anbalagan33@gmail.com>
2025-10-07 16:45:18 +0530

    checking

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
 
-// GET all tasks
+
+
+// GET all task
 router.get('/', async (req, res) => {
     const tasks = await Task.find();
     res.json({message:"Data received", tasks});
